94eaf571d2eb658316e5ba49ccab61642b996a07
amiteliy <amiteliho97@gmail.com>
2024-02-25 20:15:02 +0200

    25/02/2024 num 7

--- server/index.js
+++ server/index.js
@@ -120,10 +120,10 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
